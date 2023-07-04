170de075422e7b84ef785f64def5a0903fe8c234
Daniel Boles <dboles.src+inkscape@gmail.com>
2023-07-04 11:22:42 +0100

    extension/template—ustring.compare to string.c_str

--- src/extension/template.cpp
+++ src/extension/template.cpp
@@ -239,7 +239,7 @@ Template::Template(Inkscape::XML::Node *in_repr, ImplementationHolder implementa
 int Template::parse_visibility(const std::string &value)
 {
     int ret = 0;
-    auto values = Glib::Regex::split_simple("," , value);
+    auto values = Glib::Regex::split_simple("," , value.c_str());
     for (auto const &val : values) {
         ret |= (val == "icon") * TEMPLATE_NEW_ICON;
         ret |= (val == "list") * TEMPLATE_SIZE_LIST;
