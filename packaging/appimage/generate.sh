6ad604079a30cb0aeb2756514ada716021a37597
probonopd <27247-probono@users.noreply.gitlab.com>
2023-08-27 10:54:24 +0000

    Revert pixman (mirror did not work)

--- packaging/appimage/generate.sh
+++ packaging/appimage/generate.sh
@@ -10,13 +10,9 @@ export DEBIAN_FRONTEND=noninteractive
 # Build pixman 0.40 (needed for dithering)
 ########################################################################
 
-# git clone https://gitlab.freedesktop.org/pixman/pixman.git
-# cd pixman
-# git checkout pixman-0.40.0
-# While gitlab.freedesktop.org is down, use a mirror
-git clone https://github.com/ImageMagick/pixman
+git clone https://gitlab.freedesktop.org/pixman/pixman.git
 cd pixman
-git checkout f6c587d54159c3e32b37bc89ba409d507963789b # 0.40.0
+git checkout pixman-0.40.0
 ./autogen.sh --prefix /usr --libdir /usr/lib/x86_64-linux-gnu
 make install -j$(nproc)
 cd ..
