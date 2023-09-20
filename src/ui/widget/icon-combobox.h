999126ba5ac2a686e39fc51e21445e90104ff2f5
Daniel Boles <dboles.src+inkscape@gmail.com>
2023-09-20 16:52:41 +0100

    IconComboBox: Make build on GTK4. Split to h + cpp

--- src/ui/widget/icon-combobox.h
+++ src/ui/widget/icon-combobox.h
@@ -1,78 +1,31 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-#ifndef ICON_COMBO_BOX_SEEN_
-#define ICON_COMBO_BOX_SEEN_
+#ifndef SEEN_INKSCAPE_UI_WIDGET_ICONCOMBOBOX
+#define SEEN_INKSCAPE_UI_WIDGET_ICONCOMBOBOX
 
+#include <glibmm/refptr.h>
+#include <glibmm/ustring.h>
+#include <gtkmm/cellrendererpixbuf.h>
 #include <gtkmm/combobox.h>
-#include <gtkmm/liststore.h>
-#include <gtkmm/treemodelfilter.h>
+#include <gtkmm/treemodelcolumn.h>
 
-namespace Inkscape {
-namespace UI {
-namespace Widget {
+namespace Gtk {
+class ListStore;
+class TreeModelFilter;
+} // namespace Gtk;
+
+namespace Inkscape::UI::Widget {
 
 class IconComboBox : public Gtk::ComboBox
 {
 public:
-    IconComboBox() {
-        _model = Gtk::ListStore::create(_columns);
+    IconComboBox();
+    ~IconComboBox();
 
-        pack_start(_renderer, false);
-        _renderer.set_property("stock_size", Gtk::IconSize::NORMAL);
-        _renderer.set_padding(2, 0);
-        add_attribute(_renderer, "icon_name", _columns.icon_name);
-
-        pack_start(_columns.label);
-
-        _filter = Gtk::TreeModelFilter::create(_model);
-        _filter->set_visible_column(_columns.is_visible);
-        set_model(_filter);
-    }
-
-    void add_row(const Glib::ustring& icon_name, const Glib::ustring& label, int id) {
-        Gtk::TreeModel::Row row = *_model->append();
-        row[_columns.id] = id;
-        row[_columns.icon_name] = icon_name;
-        row[_columns.label] = ' ' + label;
-        row[_columns.is_visible] = true;
-    }
-
-    void set_active_by_id(int id) {
-        for (auto i = _filter->children().begin(); i != _filter->children().end(); ++i) {
-            const int data = (*i)[_columns.id];
-            if (data == id) {
-                set_active(i);
-                break;
-            }
-        }
-    };
-
-    void set_row_visible(int id, bool visible = true) {
-        auto active_id = get_active_row_id();
-        for (const auto & i : _model->children()) {
-            const int data = i[_columns.id];
-            if (data == id) {
-                i[_columns.is_visible] = visible;
-            }
-        }
-        _filter->refilter();
-
-        // Reset the selected row if needed
-        if (active_id == id) {
-            for (const auto & i : _filter->children()) {
-                const int data = i[_columns.id];
-                set_active_by_id(data);
-                break;
-            }
-        }
-    }
-
-    int get_active_row_id() const {
-        if (auto it = get_active()) {
-            return (*it)[_columns.id];
-        }
-        return -1;
-    }
+    void add_row(Glib::ustring const &icon_name, Glib::ustring const &label, int id);
+    void set_active_by_id(int id);
+    void set_row_visible(int id, bool visible = true);
+    int get_active_row_id() const;
 
 private:
     class Columns : public Gtk::TreeModel::ColumnRecord
@@ -97,6 +50,17 @@ private:
     Gtk::CellRendererPixbuf _renderer;
 };
 
-}}}
+} // namespace Inkscape::UI::Widget
 
-#endif
+#endif // SEEN_INKSCAPE_UI_WIDGET_ICONCOMBOBOX
+
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0)(case-label . +))
+  indent-tabs-mode:nil
+  fill-column:99
+  End:
+*/
+// vim:filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:fileencoding=utf-8:textwidth=99:
