10a4a8ea63c797ee64e166168156b7688fc7cb3d
Tavmjong Bah <tavmjong@free.fr>
2023-11-10 17:57:34 +0100

    Fix tooltips on menu items
    Step towards getting icon shifting to work.... but first need to get icons!

--- src/ui/desktop/menu-set-tooltips-shift-icons.cpp
+++ src/ui/desktop/menu-set-tooltips-shift-icons.cpp
@@ -21,11 +21,13 @@
 
 #include <glibmm/ustring.h>
 #include <gtkmm/box.h>
+#include <gtkmm/button.h>
 #include <gtkmm/cssprovider.h>
 #include <gtkmm/image.h>
 #include <gtkmm/label.h>
 
 #include "inkscape-application.h"  // Action extra data
+#include "ui/shortcuts.h"  // TEMP???
 #include "ui/util.h"
 
 // Could be used to update status bar.
@@ -58,75 +60,75 @@
 bool
 set_tooltips_and_shift_icons(Gtk::Widget &menu, bool const shift_icons)
 {
+    /*
     int width{}, height{};
 
     if (shift_icons) {
         menu.add_css_class("shifticonmenu");
         gtk_icon_size_lookup(GTK_ICON_SIZE_MENU, &width, &height);
     }
+    */
 
     bool shifted = false;
 
     // Calculate required shift. We need an example!
-    // Search for Gtk::MenuItem -> Gtk::Box -> Gtk::Image
+    // Search for GtkModelButton -> Gtk::Box, Gtk::Image (optional), Gtk::Label.
     static auto app = InkscapeApplication::instance();
     auto &label_to_tooltip_map = app->get_menu_label_to_tooltip_map();
 
-    Inkscape::UI::for_each_child(menu, [&](Gtk::Widget &child)
-    {
-        Gtk::Widget *widget = nullptr;
-        Gtk::Box *box = nullptr;
+    Inkscape::UI::for_each_child(menu, [&](Gtk::Widget &child) {
+
+        // Set tooltip.
         Glib::ustring label;
+        if (child.get_name() == "GtkModelButton") {
+            label = find_label(child);
+        }
 
-        // TODO: GTK4: Replace with appropriate widgets/nodes for PopoverMenuBar
-        if (auto const menuitem = dynamic_cast<Gtk::MenuItem *>(&child)) {
-            widget = menuitem;
-
-            auto submenu = menuitem->get_submenu();
-            if (submenu) {
-                shifted = set_tooltips_and_shift_icons(*submenu, shift_icons);
-            }
-
-            label = menuitem->get_label();
-            if (label.empty()) {
-                box = dynamic_cast<Gtk::Box *>(menuitem->get_child());
-                if (box) {
-                    label = find_label(*box);
-                }
-            }
-        } else if (child.get_name() == "modelbutton") {
-            // TODO: GTK4: layout of internal Box/Label children has changed
-            widget = &child;
-            box = dynamic_cast<Gtk::Box *>(widget->get_first_child());
-            if (box) {
-                label = find_label(*box);
+        if (!label.empty()) {
+            auto it = label_to_tooltip_map.find(label);
+            if (it != label_to_tooltip_map.end()) {
+                child.set_tooltip_text(it->second); // Set tooltip on GtkModelButton.
             }
         }
 
-        if (!widget || label.empty()) {
-            return Inkscape::UI::ForEachResult::_continue;
-        }
+        // The ModelButton contains in order: GtkBox, GtkImage (optionally), GtkLabel, GtkPopoverMenu (optionally).
+        // for (auto child2 = child.get_first_child(); child2 != nullptr; child2 = child2->get_next_sibling()) {
+        //     if (auto box = dynamic_cast<Gtk::Box*>(child2)) {
+        //         // Do something with box. We might be able to use box width to calculate shift.
+        //     }
+        //     if (auto image = dynamic_cast<Gtk::Image*>(child2)) {
+        //         // Do something with image.
+        //     }
+        //     if (auto label = dynamic_cast<Gtk::Image*>(child2)) {
+        //         // Do something with label. Redundant with above.
+        //     }
+        //     if (child2->get_name() == "GtkPopoverMenu") {
+        //         // Doesn't work as there are whole lot of widgets in the tree.
+        //         set_tooltips_and_shift_icons(*child2, shift_icons);
+        //     }
+        // }
 
-        auto it = label_to_tooltip_map.find(label);
-        if (it != label_to_tooltip_map.end()) {
-            widget->set_tooltip_text(it->second);
-        }
+        // TODO: GTK4: Figure out the kind of shift we need.
 
-        if (!shift_icons || shifted || !box) {
-            return Inkscape::UI::ForEachResult::_continue;
-        }
+        // if (!shift_icons || shifted || !box) {
+        //     return Inkscape::UI::ForEachResult::_continue;
+        // }
 
-        width += box->get_spacing();
-        auto const margin_side = widget->get_direction() == Gtk::TextDirection::RTL ? "right" : "left";
-        // TODO: GTK4: as per TODOs above.
-        auto const css_str = Glib::ustring::compose(".shifticonmenu menuitem box { margin-%1: -%2px; }"
-                                                    ".shifticonmenu modelbutton box > label:only-child { margin-%1: %2px; }",
-                                                    margin_side, width);
-        Glib::RefPtr<Gtk::CssProvider> provider = Gtk::CssProvider::create();
-        provider->load_from_data(css_str);
-        auto const display = Gdk::Display::get_default();
-        Gtk::StyleContext::add_provider_for_display(display, provider, GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
-        shifted = true;
+        // width += box->get_spacing();
+        // auto const margin_side = widget->get_direction() == Gtk::TextDirection::RTL ? "right" : "left";
+        // // TODO: GTK4: as per TODOs above.
+        // auto const css_str = Glib::ustring::compose(".shifticonmenu menuitem box { margin-%1: -%2px; }"
+        //                                             ".shifticonmenu modelbutton box > label:only-child { margin-%1: %2px; }",
+        //                                             margin_side, width);
+        // Glib::RefPtr<Gtk::CssProvider> provider = Gtk::CssProvider::create();
+        // provider->load_from_data(css_str);
+        // auto const display = Gdk::Display::get_default();
+        // Gtk::StyleContext::add_provider_for_display(display, provider, GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+        // shifted = true;
+
+        // Recurse
+        set_tooltips_and_shift_icons(child, shift_icons);
+
         return Inkscape::UI::ForEachResult::_continue;
     });
 
