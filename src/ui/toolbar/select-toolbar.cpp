lalugue/inkscape

commit a5a7857ffa01cb348686f55da98a53962a789139

Preserve toolbar icon size.
Gtk::Toolbar tries to resize all images ignoring user-specified pixel size. This is a workaround.

src/ui/toolbar/select-toolbar.cpp
@@ -455,11 +455,21 @@ SelectToolbar::on_inkscape_selection_changed(Inkscape::Selection *selection)
 
 void
 SelectToolbar::toggle_lock() {
-    if ( _lock_btn->get_active() ) {
-        _lock_btn->set_icon_name(INKSCAPE_ICON("object-locked"));
-    } else {
-        _lock_btn->set_icon_name(INKSCAPE_ICON("object-unlocked"));
+    // use this roundabout way of changing image to make sure its size is preserved
+    auto btn = static_cast<Gtk::ToggleButton*>(_lock_btn->get_child());
+    auto image = static_cast<Gtk::Image*>(btn->get_child());
+    if (!image) {
+        g_warning("No GTK image in toolbar button 'lock'");
+        return;
     }
+    auto size = image->get_pixel_size();
+
+    if ( _lock_btn->get_active() ) {
+        image->set_from_icon_name("object-locked", Gtk::ICON_SIZE_BUTTON);
+    } else {
+        image->set_from_icon_name("object-unlocked", Gtk::ICON_SIZE_BUTTON);
+    }
+    image->set_pixel_size(size);
 }
 
 void

src/widgets/desktop-widget.cpp
@@ -305,23 +305,15 @@ SPDesktopWidget::SPDesktopWidget(InkscapeWindow* inkscape_window)
         int s = prefs->getIntLimited(ToolboxFactory::tools_icon_size, min, min, max);
         ToolboxFactory::set_icon_size(tool_toolbox, s);
     };
-    auto set_ctrlbar_prefs = [=]() {
-        int min = ToolboxFactory::min_pixel_size;
-        int max = ToolboxFactory::max_pixel_size;
-        int size = prefs->getIntLimited(ToolboxFactory::ctrlbars_icon_size, min, min, max);
-        ToolboxFactory::set_icon_size(snap_toolbox, size);
-        ToolboxFactory::set_icon_size(commands_toolbox, size);
-        ToolboxFactory::set_icon_size(aux_toolbox, size);
-    };
 
     // watch for changes
     _tb_icon_sizes1 = prefs->createObserver(ToolboxFactory::tools_icon_size,    [=]() { set_toolbar_prefs(); });
-    _tb_icon_sizes2 = prefs->createObserver(ToolboxFactory::ctrlbars_icon_size, [=]() { set_ctrlbar_prefs(); });
+    _tb_icon_sizes2 = prefs->createObserver(ToolboxFactory::ctrlbars_icon_size, [=]() { apply_ctrlbar_settings(); });
     _tb_visible_buttons = prefs->createObserver(ToolboxFactory::tools_visible_buttons, [=]() { set_visible_buttons(tool_toolbox); });
 
     // restore preferences
     set_toolbar_prefs();
-    set_ctrlbar_prefs();
+    apply_ctrlbar_settings();
     set_visible_buttons(tool_toolbox);
 
     /* Canvas Grid (canvas, rulers, scrollbars, etc.) */
@@ -498,6 +490,16 @@ SPDesktopWidget::SPDesktopWidget(InkscapeWindow* inkscape_window)
     dtw->_canvas_grid->ShowCommandPalette(false);
 
     dtw->_canvas->grab_focus();
+}
+
+void SPDesktopWidget::apply_ctrlbar_settings() {
+    Inkscape::Preferences* prefs = Inkscape::Preferences::get();
+    int min = ToolboxFactory::min_pixel_size;
+    int max = ToolboxFactory::max_pixel_size;
+    int size = prefs->getIntLimited(ToolboxFactory::ctrlbars_icon_size, min, min, max);
+    ToolboxFactory::set_icon_size(snap_toolbox, size);
+    ToolboxFactory::set_icon_size(commands_toolbox, size);
+    ToolboxFactory::set_icon_size(aux_toolbox, size);
 }
 
 void SPDesktopWidget::update_statusbar_visibility() {
@@ -1289,6 +1291,10 @@ void SPDesktopWidget::setToolboxPosition(Glib::ustring const& id, GtkPositionTyp
     }
 
     layoutWidgets();
+
+    // temporary for Gtk3: Gtk toolbar resets icon sizes, so reapply them
+    // TODO: remove this call in Gtk4 after Gtk::Toolbar is eliminated
+    apply_ctrlbar_settings();
 }
 
 

src/widgets/desktop-widget.h
@@ -239,6 +239,7 @@ private:
     void rotation_populate_popup(Gtk::Menu *menu);
   //void canvas_tbl_size_allocate(Gtk::Allocation &allocation);
     void update_statusbar_visibility();
+    void apply_ctrlbar_settings();
 
 public:
     void cms_adjust_toggled();

commit 3ace5fa03c8f2a3f632faea3fadf08f5df362bfc

Fix crash on toggling lock button
Partially revert a5a7857f, which caused a crash when toggling the select
toolbar's lock button due to changed assumptions about child widgets.

Restore correct icon for lock button at startup.

src/ui/toolbar/select-toolbar.cpp
@@ -130,6 +130,7 @@ SelectToolbar::SelectToolbar(SPDesktop *desktop)
     _transform_pattern_btn.signal_toggled().connect(sigc::mem_fun(*this, &SelectToolbar::toggle_pattern));
 
     _lock_btn.signal_toggled().connect(sigc::mem_fun(*this, &SelectToolbar::toggle_lock));
+    toggle_lock();
 
     assert(desktop);
     auto *selection = desktop->getSelection();
@@ -411,21 +412,7 @@ char const *SelectToolbar::get_action_key(double mh, double sh, double mv, doubl
 
 void SelectToolbar::toggle_lock()
 {
-    // use this roundabout way of changing image to make sure its size is preserved
-    auto btn = static_cast<Gtk::ToggleButton *>(_lock_btn.get_child());
-    auto image = static_cast<Gtk::Image *>(btn->get_child());
-    if (!image) {
-        g_warning("No GTK image in toolbar button 'lock'");
-        return;
-    }
-
-    auto size = image->get_pixel_size();
-    if (_lock_btn.get_active()) {
-        image->set_from_icon_name("object-locked");
-    } else {
-        image->set_from_icon_name("object-unlocked");
-    }
-    image->set_pixel_size(size);
+    _lock_btn.set_image_from_icon_name(_lock_btn.get_active() ? "object-locked" : "object-unlocked");
 }
 
 void SelectToolbar::toggle_touch()

src/ui/widget/desktop-widget.cpp
@@ -603,10 +603,6 @@ void SPDesktopWidget::layoutWidgets()
         return Inkscape::UI::ForEachResult::_continue;
     });
 
-    // Temporary for Gtk3: Gtk toolbar resets icon sizes, so reapply them.
-    // TODO: remove this call in Gtk4 after Gtk::Toolbar is eliminated.
-    apply_ctrlbar_settings();
-
     repack_snaptoolbar();
 
     Inkscape::UI::resize_widget_children(_top_toolbars);