8a6d7ce6afa25b3bf68a9ce2f8764ef245f2fffa
PhantomzBack <yugiarora@gmail.com>
2024-03-05 05:48:57 +0000

    Update message.cpp, added missing spaces.

--- src/live_effects/parameter/message.cpp
+++ src/live_effects/parameter/message.cpp
@@ -52,10 +52,10 @@ MessageParam::param_update_default(const gchar * default_message)
 bool
 MessageParam::param_readSVGValue(const gchar * strvalue)
 {
-    if(g_strcmp0(strvalue, "")){
+    if (g_strcmp0(strvalue, "")) {
         param_setValue(strvalue);
     }
-    else{
+    else {
         // do nothing if the strvalue is empty, stick to default value
     }
     return true;
