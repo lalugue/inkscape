6a7db4cc337e11dd0c9d63c661a2a69c906a973c
PBS <pbs3141@gmail.com>
2024-04-16 12:32:14 +0900

    Make AppImage CI use continuous goappimage releases

--- packaging/appimage/generate.sh
+++ packaging/appimage/generate.sh
@@ -24,21 +24,19 @@ sed -i -e 's|^Icon=.*|Icon=org.inkscape.Inkscape|g' ./appdir/usr/share/applicati
 # Generate AppImage
 ########################################################################
 
-goappimage="appimagetool-823-x86_64.AppImage"
-wget "https://github.com/probonopd/go-appimage/releases/download/continuous/$goappimage"
-echo 46de8381cfbd142da0ac8af24f34aa8b7e1435857bac2edff3427751047e4bcb "$goappimage" | sha256sum -c
-chmod +x "$goappimage"
+goappimage_url="https://github.com/$(wget -q https://github.com/probonopd/go-appimage/releases/expanded_assets/continuous -O - | grep "appimagetool-.*-x86_64.AppImage" | head -n 1 | cut -d '"' -f 2)"
+wget -c "$goappimage_url" -O goappimage
+chmod +x goappimage
 
 # Can't use goappimage for second step since internal copy of appstreamcli is too old
-appimagekit="appimagetool-x86_64.AppImage"
-wget "https://github.com/AppImage/AppImageKit/releases/download/continuous/$appimagekit"
-echo b90f4a8b18967545fda78a445b27680a1642f1ef9488ced28b65398f2be7add2 "$appimagekit" | sha256sum -c
-chmod +x "$appimagekit"
+wget -c "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage" -O appimagekit
+echo b90f4a8b18967545fda78a445b27680a1642f1ef9488ced28b65398f2be7add2 appimagekit | sha256sum -c
+chmod +x appimagekit
 
-"./$goappimage" -s deploy ./appdir/usr/share/applications/org.inkscape.Inkscape.desktop
+./goappimage -s deploy ./appdir/usr/share/applications/org.inkscape.Inkscape.desktop
 sed -i -e 's|/usr/lib/x86_64-linux-gnu/gdk-pixbuf-.*/.*/loaders/||g' ./appdir/lib/x86_64-linux-gnu/gdk-pixbuf-*/*/loaders.cache
 cp ./appdir/usr/share/icons/hicolor/256x256/apps/org.inkscape.Inkscape.png ./appdir
-ARCH=x86_64 "./$appimagekit" ./appdir
+ARCH=x86_64 ./appimagekit ./appdir
 
 sha="$(git rev-parse --short HEAD)"
 mv Inkscape*.AppImage* "../Inkscape-$sha.AppImage"
