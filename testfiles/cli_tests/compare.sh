8ee33e4faf5b46ee61d63c2ba51dfaaa161b2681
Marc Jeanmougin <marc@jeanmougin.fr>
2024-05-06 19:14:58 +0200

    Fix multiple-style-same-text-block test
    When you have the Latin Modern Sans font installed, font-config will find it and substitute the font name
    When you don't, it does not guess it
    
    For the test to pass in both cases, we need to fix it when the font is not there.

--- testfiles/cli_tests/compare.sh
+++ testfiles/cli_tests/compare.sh
@@ -7,6 +7,8 @@ file2=$2
 test -f "${file1}" || { echo "compare.sh: First file '${file1}' not found."; exit 1; }
 test -f "${file2}" || { echo "compare.sh: Second file '${file2}' not found."; exit 1; }
 
+sed -i='' "s/LMSans..-......./'Latin Modern Sans'/" ${file1}
+
 if ! cmp "${file1}" "${file2}"; then
     echo "compare.sh: Files '${file1}' and '${file2}' are not identical'."
     exit 1
