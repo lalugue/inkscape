d9940c4ab5fc0b82c0298d45b889a90dcdf93843
Wen-Wei Kao <ltlnx@disroot.org>
2024-05-20 14:01:05 +0800

    Add missing positional argument

--- src/ui/dialog/global-palettes.cpp
+++ src/ui/dialog/global-palettes.cpp
@@ -451,7 +451,7 @@ PaletteResult load_palette(std::string const &path)
     } catch (Glib::Error const &e) {
         return {{}, compose_error(e.what())};
     } catch (...) {
-        return {{}, Glib::ustring::compose(_("Unknown error loading palette %"), utf8path)};
+        return {{}, Glib::ustring::compose(_("Unknown error loading palette %1"), utf8path)};
     }
 }
 
