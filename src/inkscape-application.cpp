bf52765c6e00ff0bc5baa4d4f5a6fc7587426b9c
Tavmjong Bah <tavmjong@free.fr>
2024-06-25 13:37:16 +0200

    Remove GC from SPDocument.
    GC was only being used to verify that the reference count was zero
    during SPDocument destruction. There is no reason for SPDocument
    to be garbage collected.
    
    In order to keep the GC dependency, a std::default_delete was added
    to the code. This resulted in undefined behavior and crashes. This
    commit removes that function.
    
    Possible fix to https://gitlab.com/inkscape/inkscape/-/issues/4839

--- src/inkscape-application.cpp
+++ src/inkscape-application.cpp
@@ -365,8 +365,6 @@ InkscapeApplication::document_close(SPDocument* document)
             std::cerr << "InkscapeApplication::close_document: Document not registered with application." << std::endl;
         }
 
-        Inkscape::GC::release(document);
-        assert(document->_anchored_refcount() == 0);
         delete document;
 
     } else {
